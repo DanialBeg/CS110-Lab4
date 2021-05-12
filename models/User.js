efa3ed3598d97a19abf7719b87e98e873368fcd3
Danial <danny00b@gmail.com>
2021-05-12 11:12:47 -0700

    Getting caught up with TA demo

---
+++ models/User.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const UserSchema = new Schema ({
+    name: {
+        type: String,
+        required: true
+    },
+    dateOfEntry: {
+        type: Date,
+        default: Date.now()
+    }
+});
+module.exports = Item = mongoose.model('user', UserSchema);
--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 // import dependencies
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('config');
 const cookieParser = require('cookie-parser');
 const hbs = require('express-handlebars');
 const path = require('path');
+const User = require('./models/User');
 
-// import handlers
-const homeHandler = require('./controllers/home.js');
-const roomHandler = require('./controllers/room.js');
+const db = config.get('mongoURI');
 
 const app = express();
-const port = 8080;
+const port = 3000;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// import handlers
+const homeHandler = require('./controllers/home.js');
+const roomHandler = require('./controllers/room.js');
 
 // If you choose not to use handlebars as template engine, you can safely delete the following part and use your own way to render content
 // view engine setup
@@ -25,11 +30,45 @@ app.set('view engine', 'hbs');
 // set up stylesheets route
 
 // TODO: Add server side code
+mongoose
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false })
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+const newUser = new User ({
+    name: 'UCR Student 2',
+});
+newUser
+  .save()
+  .then(item => console.log(item))
+  .catch(err => console.log(err));
 
 // Create controller handlers to handle requests at each endpoint
-app.get('/', homeHandler.getHome);
+app.get('/', function (req, res) {
+    User.find()
+    .sort({ date: -1 })
+    .then(items => {    res.render('home', {
+        post: {
+            author: items[0].name,
+            image: 'https://picsum.photos/500/500',
+            comments: []
+        }
+    });});
+
+});
+
+app.get('/', function (req, res) {
+    res.render('home', {
+        post: {
+            author: 'Janith Kasun',
+            image: 'https://picsum.photos/500/500',
+            comments: []
+        }
+    });
+});
+
 app.get('/:roomName', roomHandler.getRoom);
 
 // NOTE: This is the sample server.js code we provided, feel free to change the structures
 
-app.listen(port, () => console.log(`Server listening on http://localhost:${port}`));
+app.listen(3000, () => console.log(`Server listening on http://localhost:${port}`));
